96110effa31468e2de00e01ccc72785988b84457
jashwanth0712 <jashwanth0712@gmail.com>
2023-12-22 19:20:29 +0530

    added view function

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ContractExecutor from '../../components/contract'
-
+import GetDataFromContract from '../../components/GetDataFromContract'
 function page() {
   return (
-    <>
-        <ContractExecutor functionName={"0xcfb4348624dc4c16cbdac455e7302e7b2a7b9f7402627405df0842a04c467100::test19::CreateStream"} functionArguments={["0xcfb4348624dc4c16cbdac455e7302e7b2a7b9f7402627405df0842a04c467100",11,110]} />
-    </>
+    <div>
+        <ContractExecutor functionName={"0xcfb4348624dc4c16cbdac455e7302e7b2a7b9f7402627405df0842a04c467100::test19::IsRecieveStreamPresent"} functionArguments={["0xcfb4348624dc4c16cbdac455e7302e7b2a7b9f7402627405df0842a04c467100",11,11]} />
+        <GetDataFromContract functionName={"0xcfb4348624dc4c16cbdac455e7302e7b2a7b9f7402627405df0842a04c467100::test19::IsRecieveStreamPresent"} functionArguments={["0xcfb4348624dc4c16cbdac455e7302e7b2a7b9f7402627405df0842a04c467100"]} />
+    </div>
   )
 }
 
