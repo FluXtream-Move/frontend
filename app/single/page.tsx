dc55a064925abdc5760b00f1d3453a6ce90b5021
jashwanth0712 <jashwanth0712@gmail.com>
2023-12-22 16:31:21 +0530

    updated the send stream card

--- app/single/page.tsx
+++ app/single/page.tsx
@@ -4,43 +4,47 @@ import { useState } from 'react';
 import { Icon } from '@iconify/react';
 import ContractExecutor from '../../components/contract'
 import { useWallet } from '@aptos-labs/wallet-adapter-react';
-import Confetti from 'react-confetti'
-import useWindowSize from 'react-use/lib/useWindowSize'
+
+
 const SinglePage = () => {
   const [receiver, setReceiver] = useState('');
   const [token, setToken] = useState('Aptos');
   const [flow, setFlow] = useState('');
   const [duration , setDuration] = useState('');
-  const { width, height } = useWindowSize()
   const handleStream = () => {
+     // Parse flow and duration as u64
+    const parsedFlow = parseInt(flow, 10);
+    const parsedDuration = parseInt(duration, 10);
     const functionName = "0xcfb4348624dc4c16cbdac455e7302e7b2a7b9f7402627405df0842a04c467100::test19::CreateStream";
-    const functionArguments=[receiver,flow,duration];
+    const functionArguments=[receiver,parsedFlow,parsedDuration];
     console.log('Function Name:', functionName);
     console.log('Function Arguments:', functionArguments);
-    return (
-      <ContractExecutor functionName={functionName} functionArguments={functionArguments} />
-    );
+     return <ContractExecutor functionName={functionName} functionArguments={functionArguments} />
   }
   return (
     
     <div className='flex items-center justify-center min-h-screen'>
-      <Confetti
-      width={width}
-      height={height}
-    />
       <div className="bg-gray-900 text-white border border-gray-800 rounded-xl bg-gradient-to-b from-transparent to-gray-900 overflow-hidden p-7 max-w-md w-full relative">
         <button className="inline-flex items-center justify-center box-border border-0 m-0 cursor-pointer select-none align-top appearance-none text-green-600 bg-green-100 rounded-lg transition-all duration-300 ease-in-out p-2 mr-2 mb-8 self-start pointer-events-none">Send Stream</button>
         <div className="absolute top-0 right-7">
           <div className="bg-blue-600 rounded-bl-lg h-8 w-8 flex items-center justify-center shadow-md">
             <div className="text-xl font-bold text-gray-900">A</div>
           </div>
-          
         </div>
         <div className="flex flex-col space-y-4">
-          <div className="flex flex-col">
-            <label className="ml-2 mb-1 font-medium" htmlFor="receiver">Receiver Wallet Address</label>
-            <input className="shadow border rounded py-2 px-3 text-gray-900 focus:outline-none focus:shadow-outline" id="receiver" type="text" placeholder="Address" value={""} />
-          </div>
+        <div className="flex flex-col">
+          <label className="ml-2 mb-1 font-medium" htmlFor="receiver">
+            Receiver Wallet Address
+          </label>
+          <input
+            className="shadow border rounded py-2 px-3 text-gray-900 focus:outline-none focus:shadow-outline"
+            id="receiver"
+            type="text"
+            placeholder="Address"
+            value={receiver}
+            onChange={(e) => setReceiver(e.target.value)}
+          />
+        </div>
           <div className="flex flex-col md:flex-row md:space-x-4">
             <div className="w-full md:w-1/2 flex flex-col">
               <label className="ml-2 mb-1 font-medium" htmlFor="token">Flux Token</label>
@@ -55,12 +59,26 @@ const SinglePage = () => {
             </div>
             <div className="w-full md:w-1/2 flex flex-col">
               <label className="ml-2 mb-1 font-medium" htmlFor="flow">Flow Rate</label>
-              <input className="shadow border rounded py-2 px-3 text-gray-900 focus:outline-none focus:shadow-outline" id="flow" type="number" placeholder="0.00" />
+              <input
+            className="shadow border rounded py-2 px-3 text-gray-900 focus:outline-none focus:shadow-outline"
+            id="flow"
+            type="number"
+            placeholder="0.00"
+            value={flow}
+            onChange={(e) => setFlow(e.target.value)}
+          />
             </div>
           </div>
           <div className="flex flex-col">
             <label className="ml-2 mb:w-1/2 font-medium" htmlFor="receiver">Duration</label>
-            <input className="shadow border rounded py-2 px-3 text-gray-900 focus:outline-none focus:shadow-outline" id="receiver" type="text" placeholder="Duration in Seconds" value={""} />
+            <input
+            className="shadow border rounded py-2 px-3 text-gray-900 focus:outline-none focus:shadow-outline"
+            id="duration"
+            type="text"
+            placeholder="Duration in Seconds"
+            value={duration}
+            onChange={(e) => setDuration(e.target.value)}
+          />
           </div>
           <div className="flex items-center justify-end">
             <div className="text-gray-700 text-lg font-bold mr-2">/ second</div>
@@ -75,6 +93,3 @@ const SinglePage = () => {
 };
 
 export default SinglePage;
-
-
-
