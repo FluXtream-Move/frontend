d42fd3c5edfcb64079c4b55d2d32f11738387280
Satyam <na20b060@smail.iitm.ac.in>
2023-12-23 12:15:18 +0530

    tailwind config updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,53 @@ const config: Config = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
-        'neon': '#74ee15',
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
       fontFamily: {
         display: ["var(--font-sf)", "system-ui", "sans-serif"],
@@ -19,15 +63,14 @@ const config: Config = {
         poppins: ["Poppins", "sans-serif"]
       },
       animation: {
-        // Fade up and down
         "fade-up": "fade-up 0.5s",
         "fade-down": "fade-down 0.5s",
-        // Tooltip
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
         "slide-up-fade": "slide-up-fade 0.3s cubic-bezier(0.16, 1, 0.3, 1)",
         "slide-down-fade": "slide-down-fade 0.3s cubic-bezier(0.16, 1, 0.3, 1)",
       },
       keyframes: {
-        // Fade up and down
         "fade-up": {
           "0%": {
             opacity: "0",
@@ -54,7 +97,6 @@ const config: Config = {
             transform: "translateY(0px)",
           },
         },
-        // Tooltip
         "slide-up-fade": {
           "0%": {opacity: "0", transform: "translateY(6px)"},
           "100%": {opacity: "1", transform: "translateY(0)"},
@@ -63,9 +105,17 @@ const config: Config = {
           "0%": {opacity: "0", transform: "translateY(-6px)"},
           "100%": {opacity: "1", transform: "translateY(0)"},
         },
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: {height: "var(--radix-accordion-content-height)"},
+          to: {height: "0"},
+        },
       },
     },
   },
-  plugins: [nextui()],
+  plugins: [nextui(),require("tailwindcss-animate")],
 }
 export default config
