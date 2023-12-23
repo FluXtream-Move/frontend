c2e48ec743de6c3f806f33872da6fadc28edca93
jashwanth0712 <jashwanth0712@gmail.com>
2023-12-23 11:30:22 +0530

    Added laptop gif

--- components/dashboardCard.tsx
+++ components/dashboardCard.tsx
@@ -2,15 +2,18 @@
 
 import Tilt from "react-parallax-tilt";
 import Video from 'next-video';
-import dash from "/videos/dash.mp4"
+import dash from "/public/laptop.gif"
+import k from "/public/laptop.gif"
+import Image from "next/image";
 
 export default function DashboardCard() {
 
     return(
         <div className="flex">
             <Tilt>
-                <div className="w-[1100px] h-[600px] bg-gray-500 rounded-b">
-                    <Video src={dash} className=""/>
+                <div className=" rounded-b">
+                    <Image src={k} alt="FluXtream logo"  />
+                    {/* <Video src={dash} className=""/> */}
                 </div>
             </Tilt>
         </div>
